6761dd7f6a64839ed3398ba7a067f381d4e74908
emilio-stack <84351829+emilio-stack@users.noreply.github.com>
2021-09-30 15:55:09 -0600

    putting my function in

--- team script.py
+++ team script.py
@@ -5,8 +5,10 @@ from talmagehello import sayhi
 
 from krupa import print_name
 
+from emiliohello import emilio_hello
 
 print("Welcome to our collaborative program!")
 sayhi()
 
 print_name()
+emilio_hello
